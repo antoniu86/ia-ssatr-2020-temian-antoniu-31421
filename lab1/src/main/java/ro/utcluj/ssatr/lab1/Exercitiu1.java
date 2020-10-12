d0c7848bd70e291139029af229e788434dea3fd5
tony <t_tony_182@yahoo.com>
2020-10-12 20:17:11 +0300

    project files

---
+++ lab1/src/main/java/ro/utcluj/ssatr/lab1/Exercitiu1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.ssatr.lab1;
+
+/**
+ *
+ * @author antoniu
+ */
+public class Exercitiu1 {
+    public static void main(String[] args) {
+        System.out.println("Exercitiul 1");
+    }
+}
